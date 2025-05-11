9496f0e15fc0c816e7f02ef9e978b9d2efdbdf22
LimSR <tmdfuf1201@naver.com>
2025-05-11 20:01:21 +0900

    feat: 캠핑카 더미데이터 조회 기능 추가

--- src/view/user/MainFrame.java
+++ src/view/user/MainFrame.java
@@ -1,6 +1,9 @@
 package view.user;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
 
@@ -14,6 +17,22 @@ public class MainFrame extends JFrame {
         JLabel welcomeLabel = new JLabel("Camping", SwingConstants.CENTER);
         welcomeLabel.setFont(new java.awt.Font("맑은 고딕", java.awt.Font.BOLD, 20));
         add(welcomeLabel);
+        
+        // 버튼
+        JButton viewBtn = new JButton("전체 캠핑카 조회하기");
+        viewBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+
+        viewBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new CampingCarListFrame(); // 새 창 띄우기
+            }
+        });
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.add(viewBtn);
+        add(centerPanel, BorderLayout.CENTER);
+
 
         setVisible(true);
     }
